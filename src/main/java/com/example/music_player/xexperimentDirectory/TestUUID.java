3b1a3d6093d2ace8f34dcea1af31c043cf1ff217
itDarashukD <61045219+itDarashukD@users.noreply.github.com>
2021-07-06 16:34:22 +0300

    Delete TestUUID.java

--- src/main/java/com/example/music_player/xexperimentDirectory/TestUUID.java
+++
@@ -1,8 +0,0 @@
-package com.example.music_player.xexperimentDirectory;
-
-public class TestUUID {
-    public static void main(String[] args) {
-        String UUID = java.util.UUID.randomUUID().toString().replace("-","");
-        System.out.println(UUID);
-    }
-}
