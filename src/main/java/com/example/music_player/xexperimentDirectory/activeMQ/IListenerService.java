4851b1283ed20eac3d0218892ff1dbda9464487c
itDarashukD <61045219+itDarashukD@users.noreply.github.com>
2021-07-06 16:33:12 +0300

    Delete IListenerService.java

--- src/main/java/com/example/music_player/xexperimentDirectory/activeMQ/IListenerService.java
+++
@@ -1,7 +0,0 @@
-//package com.example.music_player.tetsPlayMusic.activeMQ;
-//
-//import com.example.music_player.entity.Source;
-//
-//public interface IListenerService {
-//    void getFileBySourceFromListener(Source sourceFromListener);
-//}
