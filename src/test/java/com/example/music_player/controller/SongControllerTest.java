c15d18f9f050053bbd6c61550360963d0ffd4f86
Dzmitry_Darashuk <itDarashukd@gmail.com>
2021-07-16 12:16:26 +0300

    test with sjnar and Jacoco

--- src/test/java/com/example/music_player/controller/SongControllerTest.java
+++ src/test/java/com/example/music_player/controller/SongControllerTest.java
@@ -1,11 +1,46 @@
 package com.example.music_player.controller;
 
+import com.example.music_player.MusicPlayerApplication;
+import com.example.music_player.service.AlbumService;
+import com.example.music_player.service.ISongService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.catalina.Server;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTypeExcludeFilter;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Server.class)
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = MusicPlayerApplication.class)
+
+@TypeExcludeFilters(WebMvcTypeExcludeFilter.class)
+@AutoConfigureCache
+@AutoConfigureWebMvc
+
 class SongControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ISongService songService;
 
     @Test
     void getAll() {
